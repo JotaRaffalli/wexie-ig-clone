066a890273bafe7bb21a85ff74303e827e4dcff4
Rocco <roccotc@gmail.com>
2023-02-12 22:47:41 -0300

    feat: Creación de Profile en Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useQuery } from "react-query";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import ApiService from "../../services/api.service";
+import Me from "../Me";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const {
+    isLoading,
+    error,
+    data: profileResponse,
+  } = useQuery("me", () => ApiService.fetchMe());
+
+  return (
+    <div>
+      {isLoading || !profileResponse ? (<Skeleton width={100} height={100} count={1} className="mb-4" />) 
+      : <Me {...profileResponse.data} />}
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -5,18 +5,33 @@ export interface ApiResponse<D> {
 }
 
 export interface IPostData {
-      id: string;
-      user: string;
-      username: string;
-      img: string;
-      caption: string;
-      likes: number;
+  id: string;
+  user: string;
+  username: string;
+  img: string;
+  caption: string;
+  likes: number;
+}
+
+export interface IProfileData {
+  id: string;
+  name: string;
+  lastName: string;
+  username: string;
+  likedPictures: string[];
 }
 
 class ApiService {
 
- static async fetchPosts() : Promise<ApiResponse<IPostData[]>>  {
+  static async fetchPosts() : Promise<ApiResponse<IPostData[]>>  {
     const url = `${API_URL}/posts`;
+    const result = await fetch(url);
+    const resultJson = await result.json();
+    return resultJson;
+  };
+
+  static async fetchMe() : Promise<ApiResponse<IProfileData>>  {
+    const url = `${API_URL}/me`;
     const result = await fetch(url);
     const resultJson = await result.json();
     return resultJson;
